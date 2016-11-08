172d00b19e9f4c3c8f8f23dbc641ee934f7755ee
PC\Usuario <fsperonip@hotmail.com>
2016-11-07 21:42:29 -0300

    Script de BD para verificar, paquete persistencia hecho

---
+++ BD_Obligatorio_Parte2.sql
@@ -0,0 +1,129 @@
+/*
+SQLyog Community v11.52 (32 bit)
+MySQL - 5.7.16-log : Database - obligatorio_disapp
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`obligatorio_disapp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `obligatorio_disapp`;
+
+/*Table structure for table `administradores` */
+
+DROP TABLE IF EXISTS `administradores`;
+
+CREATE TABLE `administradores` (
+  `oid` INT(11) NOT NULL,
+  `nombre` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `nombreCompleto` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`nombre`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Data for the table `administradores` */
+
+
+
+/*Table structure for table `jugadores` */
+
+DROP TABLE IF EXISTS `jugadores`;
+
+CREATE TABLE `jugadores` (
+  `oid` INT(11) NOT NULL,
+  `nombre` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `nombreCompleto` VARCHAR(50) NOT NULL,
+  `saldo` DECIMAL(12,2) NOT NULL,
+  PRIMARY KEY (`nombre`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Data for the table `jugadores` */
+
+
+
+/*Table structure for table `parametros` */
+
+DROP TABLE IF EXISTS `parametros`;
+
+CREATE TABLE `parametros` (
+  `Nombre` VARCHAR(50) NOT NULL,
+  `Valor` INT(11) NOT NULL,
+  PRIMARY KEY (`Nombre`)
+) ENGINE=MYISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `parametros` */
+
+INSERT  INTO `parametros`(`Nombre`,`Valor`) VALUES ('oid',100);
+
+/*Table structure for table `partida` */
+
+DROP TABLE IF EXISTS `partida`;
+
+CREATE TABLE `partida` (
+  `oid` INT(11) NOT NULL,
+  `codPartida` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombreJugador1` VARCHAR(50) NOT NULL,
+  `nombreJugador2` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`codPartida`),
+  KEY `FK_Jugador1` (`nombreJugador1`),
+  KEY `FK_Jugador2` (`nombreJugador2`),
+  CONSTRAINT `FK_Jugador1` FOREIGN KEY (`nombreJugador1`) REFERENCES `jugadores` (`nombre`),
+  CONSTRAINT `FK_Jugador2` FOREIGN KEY (`nombreJugador2`) REFERENCES `jugadores` (`nombre`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Data for the table `partida` */
+
+/*Table structure for table `movimiento` */
+
+DROP TABLE IF EXISTS `movimiento`;
+
+CREATE TABLE `movimiento` (
+  `oid` INT(11) NOT NULL,
+  `numeroMovimiento` INT(11) NOT NULL AUTO_INCREMENT,
+  `codPartida` INT(11) NOT NULL,
+  `ganador` VARCHAR(50) NOT NULL,
+  `fecha` DATE NOT NULL,
+  `pozo` DECIMAL(12,2) NOT NULL,
+  `nombreJugador` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`numeroMovimiento`,`codPartida`),
+  KEY `FK_Partida` (`codPartida`),
+  KEY `FK_JugadorGanador` (`ganador`),
+  KEY `FK_Jugador` (`nombreJugador`),
+  CONSTRAINT `FK_Jugador` FOREIGN KEY (`nombreJugador`) REFERENCES `jugadores` (`nombre`),
+  CONSTRAINT `FK_JugadorGanador` FOREIGN KEY (`ganador`) REFERENCES `jugadores` (`nombre`),
+  CONSTRAINT `FK_Partida` FOREIGN KEY (`codPartida`) REFERENCES `partida` (`codPartida`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Data for the table `movimiento` */
+
+/*Table structure for table `fichastableromovimiento` */
+
+DROP TABLE IF EXISTS `fichastableromovimiento`;
+
+CREATE TABLE `fichastableromovimiento` (
+  `oid` INT(11) NOT NULL,
+  `numroFichaJugada` INT(11) NOT NULL AUTO_INCREMENT,
+  `codPartida` INT(11) NOT NULL,
+  `numeroMovimiento` INT(11) NOT NULL,
+  `valorDer` INT(11) NOT NULL,
+  `valorIzq` INT(11) NOT NULL,
+  PRIMARY KEY (`codPartida`,`numeroMovimiento`,`numroFichaJugada`),
+  KEY `numroFichaJugada` (`numroFichaJugada`),
+  CONSTRAINT `fichastableromovimiento_ibfk_1` FOREIGN KEY (`codPartida`, `numeroMovimiento`) REFERENCES `movimiento` (`numeroMovimiento`, `codPartida`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Data for the table `fichastableromovimiento` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ Obligatorio/src/persistencia/BaseDatos.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import modelo.Usuario;
+
+/**
+ *
+ * @author docenteFI
+ */
+public class BaseDatos {
+
+    private static BaseDatos instancia = new BaseDatos();
+    private Connection conexion;
+    private Statement sentencia;
+
+    public static BaseDatos getInstancia() {
+        return instancia;
+    }
+    private BaseDatos() {
+    }
+    public void conectar(String url,String usuario,String pass){
+        try {
+            conexion = DriverManager.getConnection(url, usuario, pass);
+            sentencia = conexion.createStatement();
+        } catch (SQLException ex) {
+            System.out.println("Error al conectarse a la base:" + ex.getMessage());
+        }
+    }
+    public void desconectar(){
+        try {
+            sentencia.close();
+            conexion.close();
+        } catch (SQLException ex) {
+        }
+    }
+    public ResultSet consultar(String sql){
+        try {
+            ResultSet rs = sentencia.executeQuery(sql);
+            return rs;
+        } catch (SQLException ex) {
+            System.out.println("Error al consultar:" + ex.getMessage());
+            System.out.println("SQL=" + sql);
+            return null;
+        }
+    }
+    public int modificar(String sql){
+        try {
+            return sentencia.executeUpdate(sql);
+        } catch (SQLException ex) {
+            System.out.println("Error al modificar:" + ex.getMessage());
+            System.out.println("SQL=" + sql);
+            return -1;
+        }
+    }
+    public boolean transaccion(String[] sqls){
+        try {
+            conexion.setAutoCommit(false); //begin transaccion
+            for(String sql:sqls){
+                if(modificar(sql)==-1){
+                    throw new SQLException("Abortar Transaccion");
+                }
+            }
+            conexion.commit(); //guardo
+            return true;
+        } catch (SQLException ex) {
+                System.out.println("Error transaccion:" + ex.getMessage());
+            try {
+                conexion.rollback();
+                System.out.println("Rollback!");
+            } catch (SQLException ex1) {
+                    //No tendria que llegar nunca a este punto
+                    System.out.println("INCENDIO!!!: " +  ex1.getMessage());
+            }
+           
+        }finally{
+            try {
+                conexion.setAutoCommit(true); //end transaccion
+            } catch (SQLException ex) {}
+        }
+        return false;
+    }
+    
+
+   
+}
